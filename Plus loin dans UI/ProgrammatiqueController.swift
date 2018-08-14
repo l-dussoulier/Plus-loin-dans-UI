220db8cdd7f7a5d22ec07579f8a2d6cc2c69b3c7
DUSSOULIER <dussoulier.louis@hotmail.fr>
2018-08-14 15:40:46 +0200

    UIImage

--- Plus loin dans UI/ProgrammatiqueController.swift
+++ Plus loin dans UI/ProgrammatiqueController.swift
@@ -13,6 +13,8 @@ class ProgrammatiqueController: UIViewController {
     
     var monPremierUIView: UIView?
     var monPremierLabel: UILabel?
+    var monPremierBouton: UIButton?
+    var maPermiereIV: UIImageView?
     
     
     override func viewDidLoad() {
@@ -30,10 +32,46 @@ class ProgrammatiqueController: UIViewController {
         monPremierLabel?.textColor = UIColor.white
         monPremierLabel?.font = UIFont(name: "Chalkduster", size: 20)
         monPremierLabel?.textAlignment = .center
-        view.addSubview(monPremierLabel!)
+        view.addSubview(monPremierLabel! )
+        
+        let rectDemonBouton = CGRect(x: view.frame.width / 2-75, y: monPremierLabel!.frame.maxY + 20, width: 150, height: 40)
+        monPremierBouton = UIButton(frame: rectDemonBouton)
+        monPremierBouton?.setTitle("Appuyer", for: UIControlState.normal)
+        monPremierBouton?.tintColor = UIColor.red
+        monPremierBouton?.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        monPremierBouton?.backgroundColor = UIColor.black
+        monPremierBouton?.layer.borderColor = UIColor.white.cgColor
+        monPremierBouton?.layer.borderWidth = 2
+        view.addSubview(monPremierBouton!)
+      
+        
+        monPremierBouton?.addTarget(self, action: #selector(boutonAppuye), for: .touchUpInside)
         
         
+        let largeur = view.frame.width - 60
+        let rectIV = CGRect(x: 30, y: (view.frame.height / 2)-(largeur / 3), width: largeur, height: largeur)
+        maPermiereIV = UIImageView(frame: rectIV)
+        maPermiereIV?.image = #imageLiteral(resourceName: "codabee")
+        maPermiereIV?.contentMode = .scaleAspectFill
         
+        maPermiereIV?.clipsToBounds = true
+        maPermiereIV?.layer.cornerRadius = maPermiereIV!.frame.width / 2
+        view.addSubview(maPermiereIV!)
+        //view.sendSubview(toBack: maPermiereIV!)
+          view.bringSubview(toFront: monPremierBouton!)
+        
+        maPermiereIV?.isUserInteractionEnabled = true
+        maPermiereIV?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageAppuye)))
+        
+        
+    }
+    
+    @objc func imageAppuye() {
+        print ("Image touchée")
+    }
+    
+    @objc func boutonAppuye() {
+        print("tu as bien appyué")
     }
     
 
